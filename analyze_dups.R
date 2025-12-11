0be2b6e230a05ba9d30d49d84e890e994bea0dd2
LiNK-NY <mramos09@gmail.com>
2025-12-11 15:53:36 -0500

    add pre mv catalog comparison

--- analyze_dups.R
+++ analyze_dups.R
@@ -5,10 +5,20 @@ dupfiles <- vapply(
     character(1)
 )
 
-catalog <- readRDS("~/data/cancerdatasci_catalog.Rds")
+## pre mv catalog
+result <- readRDS("~/test/data_catalog_v0.Rds")
+dup_result <- result[result$fnsansext %in% dupfiles, ]
 
+table(dup_result$Project.ID)
+#' TCGA-READ
+#'       937
+
+## post mv catalog
+catalog <- readRDS("~/data/cancerdatasci_catalog.Rds")
 dup_catalog <- catalog[catalog$fnsansext %in% dupfiles, ]
 
 table(dup_catalog$Project.ID)
 #' TCGA-READ
 #'       772
+
+## difference of 165 files
